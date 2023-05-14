043cb593c7f6c527dc4c126021e485c7a230fc3a
JoseCarlosMestreMartin <josecarlosmestremartin@gmail.com>
2023-05-13 22:15:17 -0300

    realizados los models de la db

---
+++ api/src/models/Activity.js
@@ -0,0 +1,44 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define(
+    "activity",
+    {
+      idActivity: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      difficulty: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          min: 1,
+          max: 5,
+        },
+        defaultValue: 3,
+      },
+      duration: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 1,
+        get() {
+          const durationValue = this.getDataValue("duration");
+          return `${durationValue} hora(s)`;
+        },
+      },
+      season: {
+        type: DataTypes.ENUM(["Verano", "Otoño", "Invierno", "Primavera"]),
+        allowNull: false,
+      },
+    },
+    { timestamps: false }
+  );
+};
--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -1,12 +1,50 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('country', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "country",
+    {
+      idCountry: {
+        type: DataTypes.STRING(3),
+        primaryKey: true,
+        allowNull: false,
+      },
+
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      imgFlag: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      continent: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      capital: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: "Sin registro",
+      },
+      subregion: {
+        type: DataTypes.STRING,
+        defaultValue: "Sin registro",
+      },
+      area: {
+        type: DataTypes.FLOAT,
+        get() {
+          const areaValue = this.getDataValue("area");
+          return !areaValue ? `0 km2` : `${areaValue} km2`;
+        },
+      },
+      population: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
     },
-  });
+    { timestamps: false }
+  );
 };
