63801d5e45af6f8a419ed66f2f1d47e34503d952
JoseCarlosMestreMartin <josecarlosmestremartin@gmail.com>
2023-05-19 17:51:28 -0300

    rrespondiendo a get all countries

--- api/src/controllers/controlerCountries.js
+++ api/src/controllers/controlerCountries.js
@@ -1,2 +1,39 @@
 const axios = require("axios");
-const {Activity, Country} = require("../db");
+const { Activity, Country } = require("../db");
+
+const getCountriesAll = async () => {
+  let matCountries = await Country.findAll();
+
+  if (matCountries && matCountries.length > 0) {
+    return matCountries;
+  } else {
+    ////
+    let aux = (await axios.get("https://restcountries.com/v3/all")).data.map(
+      (e) => {
+        let aux = {
+          id: e.cca3,
+          name: e.name.common,
+          imgFlag: e.flags[1],
+          continent: e.continents[0],
+          capital: e.capital ? e.capital[0] : "Not found",
+          subregion: e.subregion ? e.subregion : "Not found",
+          area: e.area,
+          population: e.population,
+        };
+
+        if (aux.capital == "Papeetē") aux.capital = "Papeete";
+        if (aux.capital == "Chișinău") aux.capital = "Chisinau";
+
+        return aux;
+      }
+    );
+    //cargar en la db
+    matCountries = await Country.bulkCreate(aux);
+
+    ////
+  }
+
+  return matCountries;
+};
+
+module.export = {getCountriesAll,};
--- api/src/handlers/handlersCountries.js
+++ api/src/handlers/handlersCountries.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const { Country, Activity } = require("../db");
+const {getCountriesAll} = require("../controllers/controlerCountries");
 
 const handlerGetCountriesByNameByQuery = async (req, res, next) => {
   const name = req.query.name;
@@ -12,41 +13,35 @@ const handlerGetCountriesByNameByQuery = async (req, res, next) => {
   } else {
     //Obtiene un arreglo de objetos, donde cada objeto es un país con toda su información.
 
-    const matOfCountries = (
-      await axios.get("https://restcountries.com/v3/all")
-    ).data.map((e) => {
-      let aux = {
-        id: e.cca3,
-        name: e.name.common,
-        imgFlag: "americano", //e.flags[1],
-        continent: e.continents[0],
-        // capital: e.capital ? e.capital[0] : "Not found Capital", //e.capital[0] : "Not found Capital",
-        subregion: "americano", //e.subregion,
-        area: 10, //e.area,
-        population: 22, //e.population,
-      };
-      if(e.capital && e.capital.length>0 && (typeof e.capital[0] == "string")){
-        aux.capital = e.capital[0];
-        if (aux.capital == "Papeetē") aux.capital = "Papeete";
-        if (aux.capital == "Chișinău") aux.capital = "Chisinau";
-      }else{
-        aux.capital = "Not found Capital";
-      } 
+    const matOfCountries = await getCountriesAll();
+    // const matOfCountries = (
+    //   await axios.get("https://restcountries.com/v3/all")
+    // ).data.map((e) => {
+    //   let aux = {
+    //     id: e.cca3,
+    //     name: e.name.common,
+    //     imgFlag: e.flags[1],
+    //     continent: e.continents[0],
+    //     capital: e.capital ? e.capital[0] : "Not found",
+    //     subregion: e.subregion ? e.subregion : "Not found",
+    //     area: e.area,
+    //     population: e.population,
+    //   };
 
-      return aux;
-    });
-    //cargar en la db
-    // console.log(
-    //   "matOfCountries.data:---------------------------------------------"
-    // );
-    // console.log(matOfCountries);
+    //   if (aux.capital == "Papeetē") aux.capital = "Papeete";
+    //   if (aux.capital == "Chișinău") aux.capital = "Chisinau";
+
+    //   return aux;
+    // });
+    // //cargar en la db
     // await Country.bulkCreate(matOfCountries);
-    for (let i = 0; (i < matOfCountries.length); i++) {
-      await Country.create(matOfCountries[i]);  
-    console.log("i: ",i);
-    console.log(matOfCountries[i].capital);
-    }
- 
+
+    // for (let i = 0; (i < matOfCountries.length); i++) {
+    //   await Country.create(matOfCountries[i]);
+    // console.log("i: ",i);
+    // console.log(matOfCountries[i].capital);
+    // }
+
     // return res.send("📍 GET | /countries All  atravez de handler");
 
     return res.status(200).json({ matOfCountries });
