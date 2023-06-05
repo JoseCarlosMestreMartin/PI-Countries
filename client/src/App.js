e311d1683d84e272c5c3df798350005cb080ab6a
JoseCarlosMestreMartin <josecarlosmestremartin@gmail.com>
2023-06-05 17:52:59 -0300

    en las rutas de react

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
-
+import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from './Components/LandingPage/LandingPage';
+// import Home from './Components/Home/Home';
+// import Detail from './Components/Detail/Detail';
+// import FormCreate from './Components/FormCreate/FormCreate';
 function App() {
   return (
     <div className="App">
-      <h1>Henry Countries</h1>
+      {/* <h1>Henry Countries</h1> */}
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        {/* <Route path="/Home" element={<Home />} />
+        <Route path="/Detail/:id" element={<Detail />} />
+        <Route path="/Form" element={<FormCreate />} /> */}
+      </Routes>
     </div>
   );
 }
